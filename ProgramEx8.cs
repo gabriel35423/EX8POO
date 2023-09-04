209171856c82270f96c7854b555886da24dbe771
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:37:32 -0300

    Add files via upload

---
+++ ProgramEx8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaaPOOex8
+{
+    internal class Maior
+    {
+        private double x;
+        private double y;
+
+        public void setx(double d)
+        {
+            x = d;
+        }
+        public void sety(double d)
+        {
+            y = d;
+        }
+        public double getx()
+        {
+            return x;
+        }
+        public double gety()
+        {
+            return y;
+        }
+        public void calcular() 
+        {
+            if (x < y)
+                Console.WriteLine("y maior que x", x, y);
+            else
+                if (x > y)
+                Console.WriteLine("x maior que y");
+        }
+    }
+}
